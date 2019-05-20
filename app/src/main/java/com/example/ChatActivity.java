f7897c99434acf2eff27d403c9a6e7a3493781f2
d0800556 <50147650+d0800556@users.noreply.github.com>
2019-05-20 15:10:43 +0800

    介面修改

--- app/src/main/java/com/example/ChatActivity.java
+++ app/src/main/java/com/example/ChatActivity.java
@@ -5,15 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ChatActivity extends AppCompatActivity {
     private LinearLayout bar;
+    private TextView tv1;
+    private EditText et1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         bar = (LinearLayout)findViewById(R.id.bar);
+        tv1 = (TextView)findViewById(R.id.tv1) ;
+        et1 = (EditText)findViewById(R.id.et1) ;
         bar.bringToFront();
     }
 
@@ -40,6 +46,12 @@ public class ChatActivity extends AppCompatActivity {
         Intent intent = new Intent();
         intent.setClass(ChatActivity.this, CallActivity.class);
         startActivity(intent);
+    }
+
+    public void send_text (View v)
+    {
+        tv1.setText(et1.getText());
+        tv1.setVisibility(View.VISIBLE);
     }
 
 }
